04192045aba999bdc8d91e59088159098405dd20
neyugn291 <dhnguyen243@gmail.com>
2025-04-17 17:19:30 +0700

    add some models

--- HeThongTiemChung/HeThongTiemChung/AppTiemChung/admin.py
+++ HeThongTiemChung/HeThongTiemChung/AppTiemChung/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Vaccine, VaccineType, User
+from .models import Vaccine, VaccineType, User, InjectionSite, InjectionSchedule, VaccinationRecord, Appointment
 
 admin.site.register(Vaccine)
 admin.site.register(VaccineType)
 admin.site.register(User)
+
+admin.site.register(InjectionSite)
+admin.site.register(InjectionSchedule)
+admin.site.register(VaccinationRecord)
+admin.site.register(Appointment)
