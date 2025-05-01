ef2638686c70c919a6a64db1c5a420659f414769
neyugn291 <dhnguyen243@gmail.com>
2025-05-01 23:36:46 +0700

    debug

--- HeThongTiemChung/HeThongTiemChung/HeThongTiemChung/urls.py
+++ HeThongTiemChung/HeThongTiemChung/HeThongTiemChung/urls.py
@@ -16,8 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('', include('AppTiemChung.urls')),
     path('admin/', admin.site.urls),
-
+    re_path(r'^ckeditor/',
+            include('ckeditor_uploader.urls')),
 ]
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
