b58a4cdf6d4e8d53d2df84b11c78d447ccfa274a
neyugn291 <dhnguyen243@gmail.com>
2025-05-02 21:27:52 +0700

    stats

--- HeThongTiemChung/HeThongTiemChung/HeThongTiemChung/urls.py
+++ HeThongTiemChung/HeThongTiemChung/HeThongTiemChung/urls.py
@@ -14,17 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include, re_path
-from debug_toolbar.toolbar import debug_toolbar_urls
-
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include, re_path
+from AppTiemChung.admin import admin_site
 
 
 urlpatterns = [
     path('', include('AppTiemChung.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
     re_path(r'^ckeditor/',
             include('ckeditor_uploader.urls')),
 ]
