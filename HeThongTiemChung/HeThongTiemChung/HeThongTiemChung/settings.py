b90025043267aa4d3597ff1ef5cf0c4ba3cd9abc
neyugn291 <dhnguyen243@gmail.com>
2025-05-15 13:23:51 +0700

    email

--- HeThongTiemChung/HeThongTiemChung/HeThongTiemChung/settings.py
+++ HeThongTiemChung/HeThongTiemChung/HeThongTiemChung/settings.py
@@ -189,8 +189,12 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'dhnguyen243@gmail.com'  # Thay bằng email thật
 EMAIL_HOST_PASSWORD = 'iqhm vaao xybp qcjp'  # Dùng app password, KHÔNG dùng password tài khoản
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
